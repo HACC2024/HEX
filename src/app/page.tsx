2da4a6bcfee1851ffaf0f4d9a2de100492c03636
Ralph Jhon Ramos <22ralph.ramos@gmail.com>
2024-10-17 11:37:21 -1000

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles.css'; 
 import { Container, Nav, NavDropdown, Navbar, Row, Col } from "react-bootstrap";
-import { Facebook, Twitter, Pinterest, Instagram, HouseFill, Search, PersonFill, Cart } from 'react-bootstrap-icons';
-import Link from 'next/link'; // Use Next.js Link
+import { Facebook, Twitter, PeopleFill, Pinterest, Instagram, HouseFill, Search, PersonFill, Cart, BusFrontFill, Book  } from 'react-bootstrap-icons';
 
 const TopMenu = () => (
     <Navbar bg="light" expand="lg">
@@ -15,6 +14,7 @@ const TopMenu = () => (
                 <Nav.Link><Instagram/></Nav.Link>
             </Nav>
             <Nav className="justify-content-end">
+                <Nav.Link href="#Introduction">Introduction</Nav.Link>
                 <Nav.Link><HouseFill/> </Nav.Link>
                 <Nav.Link><Search/> </Nav.Link>
                 <Nav.Link><PersonFill/> </Nav.Link>
@@ -28,33 +28,65 @@ const TopMenu = () => (
 );
 
 const HomeImage = () => (
-    <Container className="HomeImageCt">
-        {/* Add your image here if needed */}
-    </Container>
+    <div className="HomeImageCt p-0 m-0 d-flex justify-content-center align-items-center">
+        <h1><strong>HAWAII OPEN DATA</strong></h1>
+    </div>
+);
+
+
+const Categories = () => (
+    <div className="CatDiv">
+        <h1 className="px-5 pt-3">Categories</h1>
+        <hr/>
+        <Row>
+            <Col className="d-flex flex-column align-items-center">
+                <Nav.Link href="/categories.html" className="text-center">
+                    <div className="CatIcons">
+                        <PeopleFill className="fs-1"/>
+                        <strong>Community</strong>
+                    </div>
+                </Nav.Link>
+            </Col>
+            <Col className="d-flex flex-column align-items-center">
+                <Nav.Link href="/categories.html" className="text-center">
+                    <div className="CatIcons">
+                        <BusFrontFill className="fs-1"/>
+                        <strong>Transportation</strong>
+                    </div>
+                </Nav.Link>
+            </Col>
+            <Col className="d-flex flex-column align-items-center">
+                <Nav.Link href="/categories.html" className="text-center">
+                    <div className="CatIcons">
+                        <Book className="fs-1"/>
+                        <strong>School</strong>
+                    </div>
+                </Nav.Link>
+            </Col>
+        </Row>
+    </div>
 );
 
 const IntroContainer = () => (
-    <h1>Introduction</h1>
-);
-
-const Categories = () => (
-    <>
-        <h1>Categories</h1>
+    <div id="Introduction"className="IntroDiv">
+        <h1 className="px-5 pt-3">Introduction</h1>
         <hr />
         <Row>
-            <Col>
-                <Link href="/categories.html">Community</Link>
-            </Col>
-            <Col>
-                <Link href="/categories.html">Transportation</Link>
-            </Col>
-            <Col>
-                <Link href="/categories.html">School</Link>
+            <Col className="d-flex flex-column align-items-center">
+                <p>This is our introduction/how it works/ how it works.
+                <br/>
+                This is our introduction/how it works/ how it works.<br/>
+                This is our introduction/how it works/ how it works.<br/>
+                This is our introduction/how it works/ how it works.<br/>
+                This is our introduction/how it works/ how it works.<br/>
+                This is our introduction/how it works/ how it works.
+                </p>
             </Col>
         </Row>
-    </>
+   
+    </div>
+    
 );
-
 export default function Home() {
     return (
         <main>
