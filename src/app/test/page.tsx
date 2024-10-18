666f2463e288056de06a907dc385b922dcfa7b2d
hoangv11 <victorhoang11@gmail.com>
2024-10-17 15:41:16 -1000

    update page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import "../../styles/styles.css";
 
 const TestPage = () => {
-  return (<div><h1>This is the Test AI page.</h1></div>);
+  return (
+  <div>
+    <h1>This is the Test AI page.</h1>
+  </div>);
 };
 
 export default TestPage;
