9a810c3922fabfa2b1727f7426d54e44eb2365ba
Lionel Derrick Roxas <lionelroxas80@gmail.com>
2024-10-25 01:40:16 -1000

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,9 +20,6 @@ const Navbar: React.FC = () => {
         <li>
           <Link href="test">Chatbots</Link>
         </li>
-        <li>
-          <Link href="test">Chatbots</Link>
-        </li>
       </ul>
     </nav>
   );
